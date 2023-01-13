e907c2af3fb1ba01e088ff3614d307dc64b42a2c
yonesuke <13e.e.c.13@gmail.com>
2023-01-13 12:10:25 +0900

    write setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jaxkuramoto",
+    version="0.0.1",
+    install_requires=[
+        "jax",
+    ],
+    author="Ryosuke Yoneda",
+    author_email="13e.e.c.13@gmail.com",
+    description="JAX implementation of Kuramoto model.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yonesuke/jaxkuramoto",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
