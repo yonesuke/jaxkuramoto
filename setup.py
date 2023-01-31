cc75b1aefd6c9db22ef2679c484ffe58f836920d
yonesuke <13e.e.c.13@gmail.com>
2023-01-31 20:34:10 +0900

    add several versions

--- setup.py
+++ setup.py
@@ -16,7 +16,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/yonesuke/jaxkuramoto",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
